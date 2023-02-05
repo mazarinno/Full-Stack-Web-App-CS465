cddc82080343637f04b4a284ce7ecae9d0dbc6e2
mazarinno <mcsfakianos@gmail.com>
2023-02-05 04:18:38 -0500

    MODULE5

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    modelBaseDirectory: "app_server/database/models", // model directory name
+    modelBaseDirectory: "app_api/database/models", // model directory name
     models: ["*.js", "!db.js"], // model matches
     data: "data", // data directory
     db: "mongodb://127.0.0.1:27017/travlr" // db connection url
